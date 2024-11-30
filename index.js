8c6e0cedc2b6b5558193a4c5547a823a46965a88
Thimash kavinda <153605024+Thimashkavinda@users.noreply.github.com>
2024-11-30 20:52:29 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");  
+const { Server } = require("socket.io");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-reportWebVitals();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+
+  
+    socket.on("join_room", (room) => {
+        socket.join(room);
+        console.log(`User with ID: ${socket.id} joined room: ${room}`);
+    });
+
+    socket.on("send_message", (data) => {
+        socket.to(data.room).emit("recive_message", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.id);
+    });
+});
+
+server.listen(3001, () => {
+    console.log("SERVER RUNNING");
+});
